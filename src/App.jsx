b0794c848c70f72963677a635a0a3f212e6aa2d5
sami <you@example.com>
2020-10-19 16:37:37 +0200

    one

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigations';
+
+
+
+
+
+import './App.scss';
+
+function App() {
+  return (
+      <div>
+        <Navigation />
+      </div>
+        
+       
+       
+     
+  
+  );
+}
+
+export default App;
