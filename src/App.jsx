dc760c0575cf4b979c41989be49130edae1fde32
sami <dcil142@samiti.speedport.ip>
2020-10-26 10:46:36 +0100

    components-added with About,Contact & Donate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import RichTextEditor from './component/RichTextEditor';
+import AboutWiki from './component/AboutWiki';
+import Contact from './component/Contact';
+import Donate from './component/Donate'
+
+
+
+
+
+
+
+
+
+
 
 import './App.scss';
 
---
+++ src/component/AboutWiki.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutWiki() {
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+export default AboutWiki
