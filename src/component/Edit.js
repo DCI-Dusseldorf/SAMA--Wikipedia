384b4bdf6948851fbe7a410d96d8e445a5d7ac19
sami <dcil142@samiti.speedport.ip>
2020-10-27 20:34:53 +0100

    edititng

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -1,16 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import ReactSummernote from "react-summernote";
 
 function Edit({ articles, setArticles }) {
   const { id } = useParams();
   const index = articles.findIndex((article) => article.id === id);
   const article = articles[index];
   const titleRef = useRef(null);
-  const descRef = useRef(null);
+  const [desc, setDesc] = useState(article.description);
+  const addImage = ([file]) => {
+    const reader = new FileReader();
+    reader.onloadend = () => ReactSummernote.insertImage(reader.result);
+    reader.readAsDataURL(file);
+  };
+
+  const onDescChange = (data) => {
+    setDesc(data);
+  };
 
   const onEditArticle = () => {
     article.title = titleRef.current.value;
-    article.description = descRef.current.value;
+    article.description = desc;
     setArticles([...articles]);
   };
 
@@ -18,11 +28,28 @@ function Edit({ articles, setArticles }) {
     <div>
       <h1>Add</h1>
       <input defaultValue={article.title} ref={titleRef} placeholder="Title" />
-      <input
-        defaultValue={article.description}
-        ref={descRef}
-        placeholder="description"
+      <ReactSummernote
+        onInit={() => {
+          const editArea = document.querySelector(".note-editable");
+          editArea.innerHTML = Object.values({ desc });
+        }}
+        options={{
+          height: 350,
+          dialogsInBody: true,
+          toolbar: [
+            ["style", ["style"]],
+            ["font", ["bold", "underline", "clear", "color"]],
+            ["fontname", ["fontname"]],
+            ["para", ["ul", "ol", "paragraph"]],
+            ["table", ["table"]],
+            ["insert", ["link", "picture", "video"]],
+            ["view", ["fullscreen", "codeview"]],
+          ],
+        }}
+        onChange={onDescChange}
+        onImageUpload={addImage}
       />
+
       <Link to={`/${article.id}`}>
         <button onClick={onEditArticle}>update</button>
       </Link>
