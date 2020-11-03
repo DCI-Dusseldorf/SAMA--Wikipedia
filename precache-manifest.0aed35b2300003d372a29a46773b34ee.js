7e28ad591cce6c987024b88b6b73062374bfbc7c
sami <dcil142@samiti.speedport.ip>
2020-11-03 16:18:01 +0100

    Updates

---
+++ precache-manifest.0aed35b2300003d372a29a46773b34ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714b2134f802bbff398bbc874dbdf422",
+    "url": "/SAMA--Wikipedia/index.html"
+  },
+  {
+    "revision": "6c4a6bf7991b5d1864eb",
+    "url": "/SAMA--Wikipedia/static/css/2.bd0f6615.chunk.css"
+  },
+  {
+    "revision": "4aed18b044cc21efb9d6",
+    "url": "/SAMA--Wikipedia/static/css/main.02a37416.chunk.css"
+  },
+  {
+    "revision": "6c4a6bf7991b5d1864eb",
+    "url": "/SAMA--Wikipedia/static/js/2.978137f5.chunk.js"
+  },
+  {
+    "revision": "4278980957874d2e11c65f2d979bfe82",
+    "url": "/SAMA--Wikipedia/static/js/2.978137f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aed18b044cc21efb9d6",
+    "url": "/SAMA--Wikipedia/static/js/main.f34cb97e.chunk.js"
+  },
+  {
+    "revision": "c474072e9f0e5db051d2",
+    "url": "/SAMA--Wikipedia/static/js/runtime-main.4ccff07c.js"
+  },
+  {
+    "revision": "82fa597f29de41cd41a7c402bcf09ba5",
+    "url": "/SAMA--Wikipedia/static/media/summernote.82fa597f.ttf"
+  },
+  {
+    "revision": "c1a96d26d30d9e0b2fd33c080d88c72e",
+    "url": "/SAMA--Wikipedia/static/media/summernote.c1a96d26.woff"
+  },
+  {
+    "revision": "f4a47ce92c02ef70fc848508f4cec94a",
+    "url": "/SAMA--Wikipedia/static/media/summernote.f4a47ce9.eot"
+  },
+  {
+    "revision": "f694db69cded200e4edd999fddef81b7",
+    "url": "/SAMA--Wikipedia/static/media/summernote.f694db69.woff2"
+  }
+]);
